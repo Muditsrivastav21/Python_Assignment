6a953772b53ddae734272e8e21896a70bee50cc7
Mudit Srivastav <168171420+Muditsrivastav21@users.noreply.github.com>
2026-04-24 02:01:02 +0530

    Fix typo in median calculation comment

--- Assignment_1(23-01-25)/Cal_median_marks.py
+++ Assignment_1(23-01-25)/Cal_median_marks.py
@@ -5,7 +5,7 @@ Student_score = [56, 72, 89, 65, 47, 90, 78, 84]
 Student_score.sort()
 n = len(Student_score)
 if n % 2 == 1:
-    # If the number of scores is odd the median is the middle element
+    # If the number of scores is odd the median is the middle elemenT
     median = Student_score[n // 2]
 else:
     # If the number of scores is even the median is the average of the two middle elements
